f4ed2d5b804955006c1ff8defda3fe4542c0f62a
Andrey Devyatkin <andrey.devyatkin@fivexl.io>
2023-05-22 13:27:27 +0100

    fixes
    Signed-off-by: Andrey Devyatkin <andrey.devyatkin@fivexl.io>

--- 2-tf-and-dynamic-backend/tf.sh
+++ 2-tf-and-dynamic-backend/tf.sh
@@ -8,8 +8,9 @@ export TF_TERRAFORM_EXECUTABLE=${TF_TERRAFORM_EXECUTABLE:-terraform}
 export REPO_NAME=$(basename $(git rev-parse --show-toplevel))
 export ENVIRONMENT_ID=$(aws sts get-caller-identity --query Account --output text)
 export BUCKET="myproject-infra-${ENVIRONMENT_ID}"
-export STATE_PATH="terraform/${REPO_NAME}/main.tfstate"
+export STATE_PATH="terraform/${REPO_NAME}/${TF_STATE_FILE_NAME}"
 
+echo "Using remote state s3://${BUCKET}/${STATE_PATH}"
 terraform init -backend-config "key=${STATE_PATH}" -backend-config "bucket=${BUCKET}" -backend-config "region=${AWS_DEFAULT_REGION}"
 
 terraform "$*"
