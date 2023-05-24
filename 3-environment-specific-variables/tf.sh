56daff5f801a36a55980fae2c073aaac87141a5d
Andrey Devyatkin <andrey.devyatkin@fivexl.io>
2023-05-24 10:58:49 +0100

    correct tf.sh after DevOps Pro presentation
    Signed-off-by: Andrey Devyatkin <andrey.devyatkin@fivexl.io>

--- 3-environment-specific-variables/tf.sh
+++ 3-environment-specific-variables/tf.sh
@@ -12,7 +12,7 @@ export STATE_PATH="terraform/${REPO_NAME}/${TF_STATE_FILE_NAME}"
 export TF_DATA_DIR=".terraform.${ENVIRONMENT_ID}"
 
 echo "Using remote state s3://${BUCKET}/${STATE_PATH}"
-terraform init \
+${TF_TERRAFORM_EXECUTABLE} init \
   -backend-config "key=${STATE_PATH}" \
   -backend-config "bucket=${BUCKET}" \
   -backend-config "region=${AWS_DEFAULT_REGION}"
